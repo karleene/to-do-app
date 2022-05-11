86134d4ed6ba0529e58c36b5ab4909e57ef42078
karleene <karleenestanislao@gmail.com>
2022-05-11 16:39:01 -0500

    added task checkbox functionality

--- script.js
+++ script.js
@@ -57,22 +57,40 @@ formElement.addEventListener('submit', function (e) {
 
 } );
 
-// clicking on a task allows you to toggle between checked/unchecked (AKA done vs not done)
 
-// NOTE: this will not work because you can only add event listeners which exist in the DOM at the time of code execution
+// NOTE: this will not work because you can only add event listeners which exist in the DOM at the time of code execution. li doesn't exist yet
 // const listElements = document.querySelector('li');
 // listElements.addEventListener('click', function () {
 //     console.log('To do has been checked');
 // });
 
 // in order to attach a click event listener to the li's which don't exist on the page yet, we can use EVENT PROPAGATION to DELEGATE the click event to the ul!
+const ul = document.querySelector('ul');
+
+// tend to use ANONYMOUS FUNCTIONS in an event listener! 
+ul.addEventListener('click', function (e) {
+    
+    // the "this" keyword represents the object (ul) which owns the code which is currently running. You can't use "this" if you used an arrow function. so use an anonymous function, if you're gonna use "this".
+    // console.log(this);
+
+    // log out the event object:
+    // console.log(e);
+
+    // this will log the specific nested element within the ul that the click is occuring on
+    // console.log(e.target);
 
 
+    // as long as we've clicked on the icon, then clicking on a task allows you to toggle between checked/unchecked (AKA done vs not done)
+    if (e.target.localName === 'i') {
+        console.log('checkbox was clicked');
+        
+        // toggle between checked/unchecked (AKA done vs not done) on the target element:
+        e.target.classList.toggle('fa-square-check');
+        e.target.classList.toggle('fa-square');
+        
+    }
 
-
-
-
-
+})
 
 
 // BONUS: add a "reset" button which clears all of the TO DO's
