53976145cee9290c987db4e805cfc812023acd29
karleene <karleenestanislao@gmail.com>
2022-05-11 13:40:34 -0500

    add JS file and pseudo code

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('test');
+
+// add a submit even listener on the form 
+// stop the page from refreshing when the form is submitted 
+// only if the user has entered an actual task (AKA input is not empty) that we execute the code below:
+
+    // grab the user's to-do-item from the form input 
+    // display TO-DO on the page within an li element
+        // include a checkbox icon within the li
+    // add the li element to the ul 
+    // clicking on a task allows you to toggle between checked/unchecked (AKA done vs not done)
+
+
+
+// BONUS: add a "reset" button which clears all of the TO DO's
+// BONUS: add an "EDIT TASK" button 
+// BONUS: add a "remove task" button to each task
+// BONUS: add a congratulations alert when all of the existing TO DO's are checked off
+// BONUS: add a "take a break" message if 5 or more tasks are completed
